b7f8cf8120a400b3128510632b91ddd0d6d37fd2
Gabriel Medeiros <gblmedeiros@gmail.com>
2021-11-09 15:01:01 -0300

    TDD Whith DataProvider

--- FahrenheitToCelsius/src/FahrenheitToCelsiusPackage/FahrenheitToCelsiusTest.java
+++ FahrenheitToCelsius/src/FahrenheitToCelsiusPackage/FahrenheitToCelsiusTest.java
@@ -1,23 +1,29 @@
 package FahrenheitToCelsiusPackage;
 
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-import org.junit.*;
-import static org.junit.Assert.*;
-
-import org.junit.Test;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 public class FahrenheitToCelsiusTest {
 	FahrenheitToCelsiusJava convertor = new FahrenheitToCelsiusJava();
 	
-	@Test
-	public void convert_Fahrenheit_To_Celsius() {
+	@DataProvider(name="test-data")
+	public Object[][] dataProv(){
+		return new Object[][] {
+			{32, 0},
+			{50, 10},
+			{212, 100},
+			{-40, -40},
+			{-459.67, -273.15}
+		};
+	}
+	
+	@Test(dataProvider="test-data")
+	public void convert_Fahrenheit_To_Celsius(double args1, double args2) {
 		System.out.println("Test convert Fahrenheit_To_Celsius");
-		double value = convertor.convertFahrenheitToCelsius(32);
-		double asserts = 0;
+		double value = convertor.convertFahrenheitToCelsius(args1);
+		double asserts = args2;
 		assertTrue(value == asserts);
 	}
 }
